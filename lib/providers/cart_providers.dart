e1fe13a9ec99b60cd18f8bed892c6b37dd7cdf41
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-10 13:15:38 -0400

    update states

--- lib/providers/cart_providers.dart
+++ lib/providers/cart_providers.dart
@@ -5,16 +5,21 @@ class CartNotifier extends Notifier<Set<Product>> {
   // inital value
   @override
   Set<Product> build() {
-    return const {
-      Product(
-          id: '4',
-          title: 'Red Backpack',
-          price: 14,
-          image: 'assets/products/backpack.png'),
-    };
+    return {};
   }
 
   // methods to update state
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+  }
 }
 
 final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -42,12 +42,18 @@ class HomeScreen extends ConsumerWidget {
                   Text('\$${allProducts[index].price}'),
                   if (cartProducts.contains(allProducts[index]))
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        ref.read(cartNotifierProvider.notifier)
+                          .removeProduct(allProducts[index]);
+                      },
                       child: const Text('Remove'),
                     )
                   else
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        ref.read(cartNotifierProvider.notifier)
+                          .addProduct(allProducts[index]);
+                      },
                       child: const Text('Add to cart'),
                     ),
                 ],
